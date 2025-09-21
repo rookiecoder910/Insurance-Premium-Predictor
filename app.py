e11969e730c99ab437fb5cc2c347aac146230fbb
Manas Kumar Yadav <140251934+rookiecoder910@users.noreply.github.com>
2025-09-21 22:41:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 import pandas as pd
 
 # import the ml model
-with open('model/model.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = FastAPI()
